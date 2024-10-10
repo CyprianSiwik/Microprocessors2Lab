54df6923afcaf169ae1971eea87c32cbffed2d26
Andrew B. <andyborin@gmail.com>
2024-10-10 17:04:37 -0400

    Changed main, added interrupt, deleted micro2lab1

--- Micro2_Lab1/MainMicro2Lab1.ino
+++ Micro2_Lab1/MainMicro2Lab1.ino
@@ -25,6 +25,8 @@ const int redOn = 10;
 const int greenOn = 10;
 const int yellowOn = 3;
 
+
+
 bool durationSet = false;
 bool lightRunning = false;
 
@@ -35,6 +37,26 @@ void setup() {
   pinMode(greenLED, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   systemStartup();
+
+  //set pins as outputs
+  pinMode(13, OUTPUT);
+
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
 
 }
 
