4d60a6c49cd4515ac6e726d83067951a5269df89
Anna <aschmidt1036@gmail.com>
2024-10-31 15:38:39 -0400

    update .ino

--- Micro2_Lab2/Micro2_Lab2/Micro2_Lab2/Micro2_Lab2.ino
+++ Micro2_Lab2/Micro2_Lab2/Micro2_Lab2/Micro2_Lab2.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <MPU6050.h>
+#include <avr/interrupt.h>
 
 MPU6050 mpu;
 
@@ -11,8 +12,9 @@ int xVal, yVal;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-unsigned long previousMillis = 0;
-const long interval = 100;
+volatile bool readSensorsFlag = false;
+volatile bool buzzerOn = false;
+volatile unsigned int buzzerCount = 0;  // Counts timer interrupts for buzzer timing
 
 void setup() {
   Serial.begin(9600);
@@ -25,55 +27,83 @@ void setup() {
   if (!mpu.testConnection()) {
     while (1);
   }
+
+  // Configure Timer1 for 100ms interval
+  cli();                  // Disable global interrupts
+  TCCR1A = 0;             // Clear Timer1 settings
+  TCCR1B = 0;
+  TCNT1 = 0;              // Initialize counter
+  OCR1A = 15624;          // Set compare match register for 100ms (16MHz / (256 * 100ms) - 1)
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS12);  // Prescaler 256
+  TIMSK1 |= (1 << OCIE1A); // Enable Timer1 compare interrupt
+  sei();                  // Enable global interrupts
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    xVal = analogRead(xPin);
-    yVal = analogRead(yPin);
-
-    if (xVal > 600) {
-      Serial.println('d');
-    } else if (xVal < 400) {
-      Serial.println('a');
-    }
-
-    if (yVal > 600) {
-      Serial.println('w');
-    } else if (yVal < 400) {
-      Serial.println('s');
-    }
-
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-    if (gx > 10000) {
-      Serial.println('d');
-    } else if (gx < -10000) {
-      Serial.println('a');
-    }
-
-    if (gy > 10000) {
-      Serial.println('w');
-    } else if (gy < -10000) {
-      Serial.println('s');
-    }
+  // Handle sensor readings in the main loop
+  if (readSensorsFlag) {
+    readSensorsFlag = false;
+    readJoystick();
+    readGyro();
   }
 
+  // Check for serial input to control the buzzer
   if (Serial.available()) {
     char command = Serial.read();
     if (command == 'b') {
-      activateBuzzer();
+      buzzerOn = true;
+      buzzerCount = 0;  // Reset count to manage buzzer duration
+      tone(buzzerPin, 1000);  // Start the buzzer
     }
+  }
+
+  // Manage buzzer off timing in the main loop
+  if (buzzerOn && buzzerCount >= 5) {  // 5 counts of 100ms = 500ms
+    noTone(buzzerPin);  // Stop the buzzer
+    buzzerOn = false;
   }
 }
 
-void activateBuzzer() {
-  unsigned long buzzerStartMillis = millis();
-  while (millis() - buzzerStartMillis < 500) {
-    tone(buzzerPin, 1000);
+// Timer1 interrupt service routine for 100ms interval
+ISR(TIMER1_COMPA_vect) {
+  readSensorsFlag = true;  // Set flag to read sensors every 100ms
+
+  // Increment buzzer counter if it's on
+  if (buzzerOn) {
+    buzzerCount++;
   }
-  noTone(buzzerPin);
+}
+
+void readJoystick() {
+  xVal = analogRead(xPin);
+  yVal = analogRead(yPin);
+
+  if (xVal > 600) {
+    Serial.println('d');
+  } else if (xVal < 400) {
+    Serial.println('a');
+  }
+
+  if (yVal > 600) {
+    Serial.println('w');
+  } else if (yVal < 400) {
+    Serial.println('s');
+  }
+}
+
+void readGyro() {
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  if (gx > 10000) {
+    Serial.println('d');
+  } else if (gx < -10000) {
+    Serial.println('a');
+  }
+
+  if (gy > 10000) {
+    Serial.println('w');
+  } else if (gy < -10000) {
+    Serial.println('s');
+  }
 }
